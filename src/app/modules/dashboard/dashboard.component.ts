0e09e1cb15707d8fdedf800fd5fced1e09ce67bd
edu.moreno <x7hf6ysrqnv7oydlszech6vfhn4ugnhl6orashafx4sxgki3clsa>
2022-02-14 02:23:31 -0500

    new chart

--- src/app/modules/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/dashboard.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import DatalabelsPlugin from 'chartjs-plugin-datalabels';
 import { DashboardService } from '../services/dashboard.service';
-import { ChartData, ChartType } from 'chart.js';
+import { ChartData, ChartType, ChartConfiguration } from 'chart.js';
 import { DataTransaction } from '../model/DataTransaction';
 
 @Component({
@@ -12,19 +13,51 @@ import { DataTransaction } from '../model/DataTransaction';
 export class DashBoardComponent implements OnInit {
   public model: any = {};
   public dataTransaction: DataTransaction = new DataTransaction();
+  public pieChartPlugins = [DatalabelsPlugin];
   constructor(
     private dashboardService: DashboardService,
     private router: Router
   ) {
     this.model.listTransaction = [];
   }
-
+  public pieChartOptions: ChartConfiguration['options'] = {
+    responsive: true,
+    plugins: {
+      legend: {
+        display: true,
+        position: 'bottom',
+      },
+      // datalabels: {
+      //   formatter: (value, ctx) => {
+      //     if (ctx.chart.data.datasets) {
+      //       // return ctx.chart.data.labels[ctx.dataIndex];
+      //     }
+      //   },
+      // },
+    },
+  };
   public doughnutChartLabels: string[] = ['Data Maestra', 'Transacciones'];
   public doughnutChartData: ChartData<'doughnut'> | undefined;
   public doughnutChartType: ChartType = 'doughnut';
+
+  // PolarArea
+  public polarAreaChartLabels: string[] = [];
+  public polarAreaChartData: ChartData<'polarArea'> = {
+    labels: this.polarAreaChartLabels,
+    datasets: [
+      {
+        data: [],
+        label: 'Series 1',
+      },
+    ],
+  };
+  public polarAreaLegend = true;
+  public polarAreaChartType: ChartType = 'polarArea';
+
   ngOnInit() {
     this.getTransaction();
     this.getTransactionChart(100);
+    this.getTransactionsLastSixMonths();
   }
 
   getTransactionChart(value: any) {
@@ -57,6 +90,26 @@ export class DashBoardComponent implements OnInit {
       this.model.listTransaction = res;
       console.log(res);
     });
+  }
+
+  getTransactionsLastSixMonths() {
+    this.dashboardService
+      .getTransactionsLastSixMonths()
+      .subscribe((res: any) => {
+        console.log(res);
+        const dataLabel = res.map((el: any) => el.month);
+        const dataSet = res.map((el: any) => el.qty);
+        this.polarAreaChartLabels = dataLabel;
+        this.polarAreaChartData = {
+          labels: this.polarAreaChartLabels,
+          datasets: [
+            {
+              data: dataSet,
+              label: 'Series 1',
+            },
+          ],
+        };
+      });
   }
 
   detailTransaction(id: any) {
--- src/app/modules/services/dashboard.service.ts
+++ src/app/modules/services/dashboard.service.ts
@@ -1,28 +1,37 @@
-import { HttpClient, HttpHeaders } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { environment } from "src/environments/environment";
-import { map, catchError } from "rxjs/operators";
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { map, catchError } from 'rxjs/operators';
 
 @Injectable({
-    providedIn: 'root',
+  providedIn: 'root',
 })
-export class DashboardService{
+export class DashboardService {
+  constructor(private http: HttpClient) {}
+  getTransaction() {
+    const headers = new HttpHeaders();
+    headers.append('Access-Control-Allow-Origin', '*');
+    return this.http
+      .get(`${environment.apiUrl}/listarTransactionDashboard`, {
+        headers: headers,
+      })
+      .pipe(map((res) => res));
+  }
+  getCantTransactionMonth() {
+    const headers = new HttpHeaders();
+    headers.append('Access-Control-Allow-Origin', '*');
+    return this.http
+      .get(`${environment.apiUrl}/getCantTransactionMonth`, {
+        headers: headers,
+      })
+      .pipe(map((res) => res));
+  }
 
-    constructor(private http: HttpClient){
-
-    }
-    getTransaction(){
-        const headers = new HttpHeaders();
-        headers.append('Access-Control-Allow-Origin', '*');
-        return this.http
-        .get(`${environment.apiUrl}/listarTransactionDashboard`,{headers: headers})
-        .pipe(map(res => res));
-    }
-    getCantTransactionMonth(){
-        const headers = new HttpHeaders();
-        headers.append('Access-Control-Allow-Origin', '*');
-        return this.http
-        .get(`${environment.apiUrl}/getCantTransactionMonth`,{headers: headers})
-        .pipe(map(res => res));
-    }
+  getTransactionsLastSixMonths() {
+    const headers = new HttpHeaders();
+    headers.append('Access-Control-Allow-Origin', '*');
+    return this.http
+      .get(`${environment.apiUrl}/polarChartTransactions`, { headers: headers })
+      .pipe(map((res) => res));
+  }
 }
